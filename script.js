c74bc258374001eb7290502ad928ebafa799af86
TechByKaif <kaifalam852195@gmail.com>
2024-11-26 15:04:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector(".btn");
+    btn.addEventListener("click", () => {
+        alert("Welcome to Our Services!");
+    });
+});
